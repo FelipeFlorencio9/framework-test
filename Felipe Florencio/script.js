73412ed40c4f1b4337218971c6e8f842e6db7e8f
Home -  Felipe Florencio <FelipFlorencio9@github.com>
2023-01-26 18:46:50 -0300

    Maior parte terminado teste de tela de login com framework

--- Felipe Florencio/script.js
+++
@@ -1,8 +0,0 @@
-
-var button = Uikit.button('#button', {
-    offset: 50
-});
-document.createElement(button);
-var teste = <button class="uk-button uk-button-default"></button>;
-document.createElement(teste);
-UIkit.img(element, options);
