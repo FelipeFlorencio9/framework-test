5ee2bed84e889c3b0e84d5b7b525b3fb21c1c4bd
Home -  Felipe Florencio <FelipFlorencio9@github.com>
2023-01-26 14:59:23 -0300

    Testes com elementos do framework

---
+++ Felipe Florencio/script.js
@@ -0,0 +1,8 @@
+
+var button = Uikit.button('#button', {
+    offset: 50
+});
+document.createElement(button);
+var teste = <button class="uk-button uk-button-default"></button>;
+document.createElement(teste);
+UIkit.img(element, options);
